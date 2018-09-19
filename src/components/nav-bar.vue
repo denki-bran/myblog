d796feba49d54b3d2021de056155bdaaaeaede87
dingjy <dingjy@2345.com>
2018-09-19 19:07:10 +0800

    add needed components

---
+++ src/components/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="nav-bar">
+    </div>
+</template>
+<script>
+    export default{
+        name:'data-list',
+        data(){
+
+        }
+    }
+</script>
+<style lang="scss">
+    @import '@/assets/remcalc/_common.scss';
+</style>
